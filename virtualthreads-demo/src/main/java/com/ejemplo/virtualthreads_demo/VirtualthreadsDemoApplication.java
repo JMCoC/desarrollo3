2837eabb764bf88f59c70237960427c7788572e2
JMCoC <juan.ramirez.agudelo@correounivalle.edu.co>
2025-06-16 15:45:22 -0500

    Codelab de virtual threads

---
+++ virtualthreads-demo/src/main/java/com/ejemplo/virtualthreads_demo/VirtualthreadsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.virtualthreads_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualthreadsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualthreadsDemoApplication.class, args);
+	}
+
+}
---
+++ virtualthreads-demo/src/test/java/com/ejemplo/virtualthreads_demo/VirtualthreadsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.virtualthreads_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
